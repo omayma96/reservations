10337fb5582f651c57e8c0e00febd6199d55ef7f
omayma hajjami <omayma.hajjami@gmail.com>
2021-06-26 11:48:39 +0100

    all projet

---
+++ Services/IReservationTypeService.cs
@@ -0,0 +1,13 @@
+﻿using reservation_system.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace reservation_system.Services
+{
+    public interface IReservationTypeService
+    {
+        Task<string> GetReservationTypebyId(int ResID);
+    }
+}
